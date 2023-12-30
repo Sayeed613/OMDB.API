a37bb8cad5365b2b2637de278ad8a8d82594cdc3
Sayeed613 <Sayeedahmed90082@gmail.com>
2023-12-30 12:20:24 +0530

    movie.app

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=23361513`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=23361513`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
             displayMovieDetails(movieDetails);
